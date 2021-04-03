c3e38f380122d173126c38fc4ed54d9dadbf667a
Guillaume Hamel <guillaume.hamel@efrei.net>
2021-04-03 20:06:32 +0200

    Start design of event page

--- adIOS amigIOS/EventPage.swift
+++ adIOS amigIOS/EventPage.swift
@@ -11,7 +11,59 @@ struct EventPage: View {
     let event: Event
     
     var body: some View {
-        Text(event.title)
+        ScrollView {
+            GeometryReader { geometry in
+                Image("background")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: geometry.size.width, height: geometry.size.height)
+                    .clipped()
+            }.frame(height: 300)
+            
+            VStack {
+                ZStack(alignment: .leading) {
+                    RoundedRectangle(cornerRadius: 16, style: .continuous)
+                        .fill(Color.white)
+                        .shadow(radius: 20)
+                    Text(event.title + " with " + event.speaker)
+                        .font(.title)
+                        .foregroundColor(.black)
+                        .multilineTextAlignment(.leading)
+                }
+                .frame(height: 50)
+                
+                HStack {
+                    ZStack(alignment: .center) {
+                        RoundedRectangle(cornerRadius: 16, style: .continuous)
+                            .fill(Color.white)
+                            .shadow(radius: 20)
+                        Text("Date & Time")
+                            .font(.title3)
+                            .foregroundColor(.black)
+                        Text(event.startTime)
+                            .font(.title)
+                            .foregroundColor(.blue)
+                        Text(event.endTime)
+                            .font(.title)
+                            .foregroundColor(.blue)
+                    }
+                    .frame(height: 50)
+                    
+                    ZStack(alignment: .leading) {
+                        RoundedRectangle(cornerRadius: 16, style: .continuous)
+                            .fill(Color.white)
+                            .shadow(radius: 20)
+                        Text(event.title)
+                            .font(.title)
+                            .foregroundColor(.black)
+                            .multilineTextAlignment(.leading)
+                    }
+                    .frame(height: 50)
+                }
+            }
+            .padding(16)
+        }
+        .edgesIgnoringSafeArea(.all) // 3
     }
 }
 
