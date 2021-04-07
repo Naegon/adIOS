c834ccce8ba06fd5d36f32c59f4637714688d1b7
Guillaume Hamel <guillaume.hamel@efrei.net>
2021-04-07 12:01:27 +0200

    UI improvment

--- adIOS amigIOS/EventPage.swift
+++ adIOS amigIOS/EventPage.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Guillaume Hamel on 03/04/2021.
 //
-
 import SwiftUI
 
 struct EventPage: View {
@@ -21,47 +20,91 @@ struct EventPage: View {
             }.frame(height: 300)
             
             VStack {
-                ZStack(alignment: .leading) {
-                    RoundedRectangle(cornerRadius: 16, style: .continuous)
-                        .fill(Color.white)
-                        .shadow(radius: 20)
-                    Text(event.title + " with " + event.speaker)
+                HStack(alignment: .center) {
+                    Text(event.title)
+                        .font(.title)
+                        .bold()
+                        .foregroundColor(.black)
+                        .multilineTextAlignment(.leading)
+                    Text("with " + event.speaker)
                         .font(.title)
                         .foregroundColor(.black)
                         .multilineTextAlignment(.leading)
                 }
                 .frame(height: 50)
+                .background(RoundedRectangle(cornerRadius: 16, style: .continuous)
+                    .fill(Color.white)
+                    .shadow(radius: 10)
+                )
+                .padding(10)
+                
                 
                 HStack {
-                    ZStack(alignment: .center) {
-                        RoundedRectangle(cornerRadius: 16, style: .continuous)
-                            .fill(Color.white)
-                            .shadow(radius: 20)
+                    VStack(alignment: .center) {
+                        
                         Text("Date & Time")
                             .font(.title3)
                             .foregroundColor(.black)
                         Text(event.startTime)
-                            .font(.title)
+                            .font(.body)
                             .foregroundColor(.blue)
                         Text(event.endTime)
-                            .font(.title)
+                            .font(.body)
                             .foregroundColor(.blue)
+                            .multilineTextAlignment(.leading)
                     }
-                    .frame(height: 50)
-                    
-                    ZStack(alignment: .leading) {
+                    .frame(width :160,height: 70)
+                    .background(
                         RoundedRectangle(cornerRadius: 16, style: .continuous)
-                            .fill(Color.white)
-                            .shadow(radius: 20)
+                        .fill(Color.white)
+                        .shadow(radius: 20)
+                    )
+                    
+                    VStack(alignment: .center) {
+                        
                         Text(event.title)
                             .font(.title)
                             .foregroundColor(.black)
                             .multilineTextAlignment(.leading)
+                        Text("30 avenue de la Paix")
+                            .font(.body)
+                            .foregroundColor(.gray)
+                            .multilineTextAlignment(.leading)
                     }
-                    .frame(height: 50)
+                    .frame(height: 70)
+                    .background(RoundedRectangle(cornerRadius: 16, style: .continuous)
+                                    .fill(Color.white)
+                                    .shadow(radius: 20))
+                }
+                .padding(10)
+                
+                HStack {
+                    VStack(alignment: .center) {
+                        
+                        Text("Speaker name")
+                            .font(.title3)
+                            .foregroundColor(.black)
+                        Text("CEO at compagny")
+                            .font(.body)
+                            .foregroundColor(.gray)
+                        Text("Email : myemail@email.com")
+                            .font(.body)
+                            .foregroundColor(.black)
+                            .multilineTextAlignment(.leading)
+                        Text("Phone : +(33)4578974979")
+                            .font(.body)
+                            .foregroundColor(.black)
+                            .multilineTextAlignment(.leading)
+                    }
+                    .frame(height: 90)
+                    .background(
+                        RoundedRectangle(cornerRadius: 16, style: .continuous)
+                        .fill(Color.white)
+                        .shadow(radius: 20)
+                    )
                 }
             }
-            .padding(16)
+            .padding(10)
         }
         .edgesIgnoringSafeArea(.all) // 3
     }
